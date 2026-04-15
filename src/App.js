c620cff30ab2ca0e7788b2f149e24f54649ae2e7
Salvatore Filippone <sfilippone@gmail.com>
2026-04-15 12:37:18 -0700

    last edit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ export default function App() {
   return (
     <div>
       <Steps />
-      {/* <Steps /> */}
     </div>
   );
 }
@@ -43,9 +42,9 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <div className="message">
-            Step {step}: {messages[step - 1]}
-          </div>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
           <div className="buttons">
             <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
               <span>👈🏻</span> Previous
@@ -57,6 +56,15 @@ function Steps() {
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}:</h3>
+      {children}
+    </div>
   );
 }
 
